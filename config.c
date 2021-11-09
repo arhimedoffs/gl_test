517d31114a2956d739c16cbc60a7b3f700a6c1ab
Dmytro Kovalenko <kovalenok@elius.com.ua>
2021-11-09 16:03:57 +0200

    Add stubs for configuration functions

--- config.c
+++ config.c
@@ -1,1 +1,19 @@
 #include "config.h"
+
+#define UNUSED(x) (void)(x)
+
+int cfgAdd(const char *fName, const tCommandParam *const param) {
+    UNUSED(fName);
+    UNUSED(param);
+    return 0;
+}
+int cfgGet(const char *fName, const tCommandParam *const param) {
+    UNUSED(fName);
+    UNUSED(param);
+    return 0;
+}
+int cfgDel(const char *fName, const tCommandParam *const param) {
+    UNUSED(fName);
+    UNUSED(param);
+    return 0;
+}
--- config.h
+++ config.h
@@ -1,1 +1,22 @@
 #pragma once
+
+typedef enum tCommand {
+    CMD_EMPTY = 0,
+    CMD_ADD,
+    CMD_GET,
+    CMD_DEL,
+    CMD_NOT_EXIST
+} tCommand;
+
+typedef struct tCommandParam {
+    int slot;
+    int port;
+    int slotRange; // -1 if range is not used
+    int portRange; // -1 if range is not used
+    char option[33];
+    char value[33];
+} tCommandParam;
+
+int cfgAdd(const char *fName, const tCommandParam *const param);
+int cfgGet(const char *fName, const tCommandParam *const param);
+int cfgDel(const char *fName, const tCommandParam *const param);
